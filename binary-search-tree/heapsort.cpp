2e79040e31d4bc313a04c8edfb2217b4c8a71149
delphox60 <delphox60@gmail.com>
2023-04-18 00:48:56 +0900

    feat: implement heapsort complete

--- binary-search-tree/heapsort.cpp
+++ binary-search-tree/heapsort.cpp
@@ -7,10 +7,16 @@ int heap_size;
 
 int arr[HEAP_LENGTH];
 
+void swap(int a, int b)
+{
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+    return;
+}
+
 void max_heapify(int idx)
 {
-    int cur = arr[idx];
-
     int largest = idx;
     if (2 * idx <= heap_size && arr[2 * idx] > arr[largest])
     {
@@ -24,20 +30,45 @@ void max_heapify(int idx)
     if (largest == idx)
         return;
 
-    int tmp = arr[largest];
-    arr[largest] = arr[idx];
-    arr[idx] = tmp;
+    swap(largest, idx);
 
     max_heapify(largest);
 }
 
 void build_max_heap()
 {
-    for (int i = heap_size / 2; i >= 1; i++)
+    for (int i = heap_size / 2; i >= 1; i--)
         max_heapify(i);
+}
+
+void heapsort()
+{
+    build_max_heap();
+    while (heap_size > 0)
+    {
+        swap(1, heap_size);
+        heap_size--;
+        max_heapify(1);
+    }
 }
 
 int main()
 {
     cin >> heap_size;
+
+    int n_of_items = heap_size;
+
+    for (int i = 1; i <= heap_size; i++)
+        cin >> arr[i];
+
+    for (int i = 1; i <= heap_size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    heapsort();
+    // build_max_heap();
+
+    for (int i = 1; i <= n_of_items; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }

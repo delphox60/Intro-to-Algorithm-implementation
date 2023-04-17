029babb04a63565c69284d66960665f96e81fc5e
delphox60 <delphox60@gmail.com>
2023-04-17 21:32:54 +0900

    simple BST: implement insert method

--- binary-search-tree/simplebst.cpp
+++ binary-search-tree/simplebst.cpp
@@ -20,6 +20,42 @@ public:
         this->left = nullptr;
         this->p = nullptr;
     }
+    int getKey()
+    {
+        return this->key;
+    }
+
+    node *getRight()
+    {
+        return this->right;
+    }
+
+    node *getLeft()
+    {
+        return this->left;
+    }
+
+    node *getP()
+    {
+        return this->p;
+    }
+
+    void setKey(int key)
+    {
+        this->key = key;
+    }
+    void setRight(node *right)
+    {
+        this->right = right;
+    }
+    void setLeft(node *left)
+    {
+        this->left = left;
+    }
+    void setP(node *p)
+    {
+        this->p = p;
+    }
 };
 
 class tree
@@ -32,9 +68,49 @@ public:
     {
         this->root = nullptr;
     }
+
+    void insert(int k)
+    {
+        node n = node(k);
+        node *cur = this->root;
+        if (cur == nullptr)
+        {
+            this->root = &n;
+            return;
+        }
+        while (true)
+        {
+            if (n.getKey() < cur->getKey())
+            {
+                if (cur->getLeft() == nullptr)
+
+                    n.setP(cur);
+                cur->setLeft(&n);
+                return;
+
+                cur = cur->getLeft();
+            }
+            if (cur->getRight() == nullptr)
+            {
+                n.setP(cur);
+                cur->setRight(&n);
+                return;
+            }
+            cur = cur->getRight();
+        }
+    }
 };
 
 int main()
 {
     tree t = tree();
+
+    t.insert(3);
+    t.insert(4);
+    t.insert(31);
+    t.insert(23);
+    t.insert(98);
+    t.insert(46);
+    t.insert(42);
+    t.insert(5);
 }
